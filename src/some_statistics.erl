e09fa8f62c18c8fd999e2e5486e0830d599a8336
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2017-03-19 11:55:22 -0500

    added variance, cov, and correlation

--- src/some_statistics.erl
+++ src/some_statistics.erl
@@ -3,7 +3,8 @@
 %% API exports
 -export([count/1, sum/1, max/1, min/1, average/1, type_it/1, mode/1, histogram/1, div_con_sort/1, median/1, quartile/2,
   loop_sum/0, loop_count/0, loop_average/0, loop_mode/0, loop_max/0, loop_min/0, loop_median/0,
-  loop_quartile/0, loop_histogram/0, loop_div_con_sort/0]).
+  loop_quartile/0, loop_histogram/0, loop_div_con_sort/0, loop_var/0, var/1, loop_sample_var/0,
+  sample_var/1, loop_std/0, std/1, loop_cov/0, cov/2, zip/3, loop_sample_cov/0, sample_cov/2, loop_corr/0, corr/2]).
 
 %%====================================================================
 %% API functions
@@ -206,6 +207,99 @@ loop_median() ->
 
 median(L) ->
   quartile(L, 0.5).
+
+loop_var() ->
+  receive
+    {ReturnPid, L} -> ReturnPid ! {ok, var(L)}, loop_var() ;
+    _ -> loop_var()
+  end.
+
+var(X) ->
+  Mean = average(X),
+  Val = type_it(X),
+  var(Val, Mean).
+
+var({list, List}, Mean) ->
+  NewList = lists:map(fun (Ele) -> (Ele - Mean) * (Ele - Mean) end, List),
+  average(NewList) ;
+var({key_values, KeyVales}, Mean) ->
+  NewList = lists:map(fun ({_, Ele}) -> (Ele - Mean) * (Ele - Mean) end, KeyVales),
+  average(NewList).
+
+loop_std() ->
+  receive
+    {ReturnPid, L} -> ReturnPid ! {ok, std(L)}, loop_std() ;
+    _ -> loop_std()
+  end.
+
+std(X) ->
+  math:sqrt(var(X)).
+
+loop_sample_var() ->
+  receive
+    {ReturnPid, L} -> ReturnPid ! {ok, sample_var(L)}, loop_sample_var() ;
+    _ -> loop_sample_var()
+  end.
+
+sample_var(X) ->
+  Var = var(X),
+  {_, L} = type_it(X),
+  Length = length(L),
+  Var * (Length / (Length - 1)).
+
+zip([], [], Zipped) -> Zipped ;
+zip([H1 | T1], [H2 | T2], Zipped) -> zip(T1, T2, [{H1, H2} | Zipped]).
+
+loop_cov() ->
+  receive
+    {ReturnPid, X, Y} -> ReturnPid ! {ok, cov(X,Y)}, loop_cov() ;
+    _ -> loop_cov()
+  end.
+
+cov(X, Y) ->
+  XMean = average(X),
+  XVal = type_it(X),
+  YMean = average(Y),
+  YVal = type_it(Y),
+  cov(XVal, YVal, XMean, YMean).
+
+cov({list, XList}, {list, YList}, XMean, YMean) ->
+  ZippedCov = zip(XList, YList, []),
+  NewList = lists:map(fun ({XEle, YEle}) -> (XEle - XMean) * (YEle - YMean) end, ZippedCov),
+  average(NewList) ;
+cov(_, _, _, _) -> unmatched_error.
+
+loop_sample_cov() ->
+  receive
+    {ReturnPid, X, Y} -> ReturnPid ! {ok, sample_cov(X,Y)}, loop_sample_cov() ;
+    _ -> loop_sample_cov()
+  end.
+
+sample_cov(X, Y) ->
+  Cov = cov(X,Y),
+  {_, L} = type_it(X),
+  Length = length(L),
+  Cov * (Length/(Length - 1)).
+
+loop_corr() ->
+  receive
+    {ReturnPid, X, Y} -> ReturnPid ! {ok, corr(X,Y)}, loop_corr() ;
+    _ -> loop_corr()
+  end.
+
+corr(X, Y) ->
+  {_,XL} = type_it(X),
+  {_,YL} = type_it(Y),
+  Length = length(XL),
+  ZippedCor = zip(XL, YL, []),
+  ProductL = lists:map(fun ({XEle, YEle}) -> (XEle * YEle) end, ZippedCor),
+  SumX = sum(X),
+  SumY = sum(Y),
+  Top = Length * sum(ProductL) - (SumX * SumY),
+  XSquares = lists:map(fun (XEle) -> (XEle * XEle) end, XL),
+  YSquares = lists:map(fun (YEle) -> (YEle * YEle) end, YL),
+  Bottom = math:sqrt((Length * sum(XSquares) - (SumX * SumX)) * (Length * sum(YSquares) - (SumY * SumY))),
+  (Top / Bottom).
 
 
 %%====================================================================
